c4cc82635351053581d044d703bf723c2cc1a37d
DESKTOP/QuaNilo <anyonept911@gmail.com>
2024-04-04 12:47:37 +0100

    Improved Styling on home page && Statistics no longer on top of the wallpaper

--- resources/views/livewire/show-counter.blade.php
+++ resources/views/livewire/show-counter.blade.php
@@ -1,22 +1,22 @@
 <section class="container relative lg:mt-12">
-    <div class="relative grid md:grid-cols-4 grid-cols-1 items-center mt-8 gap-[30px] z-1">
-        <div class="counter-box text-center">
-            <h1 class="lg:text-5xl text-4xl font-bold mb-2"><span class="counter-value" data-target="{{$proposalsCount}}">{{$proposalsCount / 4}}</span>+</h1>
+    <div class="flex justify-evenly space-x-7">
+        <div class="flex-grow text-center shadow-md rounded p-1 bg-indigo-300/90 hover:bg-indigo-400/80 transition ease-in-out delay-50">
+            <h1 class="lg:text-5xl text-4xl font-mono font-semibold"><span class="counter-value" data-target="{{$proposalsCount}}">{{$proposalsCount / 4}}</span>+</h1>
             <h5 class="counter-head text-lg font-medium">{{__('Proposals')}}</h5>
         </div><!--end counter box-->
 
-        <div class="counter-box text-center">
-            <h1 class="lg:text-5xl text-4xl font-bold mb-2"><span class="counter-value" data-target="{{$winnersCount}}">{{$winnersCount / 4}}</span>+</h1>
+        <div class="flex-grow text-center shadow-md rounded p-1 bg-indigo-400/90 hover:bg-indigo-500/80 transition ease-in-out delay-50">
+            <h1 class="lg:text-5xl text-4xl font-mono font-semibold"><span class="counter-value" data-target="{{$winnersCount}}">{{$winnersCount / 4}}</span>+</h1>
             <h5 class="counter-head text-lg font-medium">{{__('Winning Proposals')}}</h5>
         </div><!--end counter box-->
 
-        <div class="counter-box text-center">
-            <h1 class="lg:text-5xl text-4xl font-bold mb-2"><span class="counter-value" data-target="{{$usersCount}}">{{$usersCount / 4}}</span>+</h1>
+        <div class="flex-grow text-center shadow-md rounded p-1 bg-indigo-500/90 hover:bg-indigo-600/80 transition ease-in-out delay-50">
+            <h1 class="lg:text-5xl text-4xl font-mono font-semibold"><span class="counter-value" data-target="{{$usersCount}}">{{$usersCount / 4}}</span>+</h1>
             <h5 class="counter-head text-lg font-medium">{{__('Citizens')}}</h5>
         </div><!--end counter box-->
 
-        <div class="counter-box text-center">
-            <h1 class="lg:text-5xl text-4xl font-bold mb-2"><span class="counter-value" data-target="{{$votesCount}}">{{$votesCount / 4}}</span>+</h1>
+        <div class="flex-grow text-center shadow-md rounded p-1 bg-indigo-600/90 hover:bg-indigo-700/80 transition ease-in-out delay-50">
+            <h1 class="lg:text-5xl text-4xl font-mono font-semibold"><span class="counter-value" data-target="{{$votesCount}}">{{$votesCount / 4}}</span>+</h1>
             <h5 class="counter-head text-lg font-medium">{{__('Votes')}}</h5>
         </div><!--end counter box-->
     </div>
--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -31,55 +31,53 @@ $wallpaper = $home->getFirstMediaUrl('wallpaper');
                     </div><!--end col-->
                 </div><!--end grid-->
             </div><!--end container-->
-            <div class="absolute bottom-5 left-1/2 transform -translate-x-1/2 mb-6">
+        </section><!--end section-->
+        <section id="what" class="relative pt-10 pb-10 mt-0 bg-gray-50 dark:bg-slate-800">
+            <div class="relative mb-8">
                 <livewire:show-counter/>
             </div>
-        </section><!--end section-->
-            <!-- End Hero -->
-            <section id="what" class="relative md:py-24 pt-0 mt-0 bg-gray-50 dark:bg-slate-800">
-                <div class="container relative">
-                    <div class="grid grid-cols-1 pb-8 text-center">
-                        @if($homeInfo = \App\Models\HomeInfo::first())
-                            <h3 class="mb-6 md:text-3xl text-2xl md:leading-normal leading-normal font-semibold">{{$homeInfo->title}}?</h3>
-                            <p class="text-slate-400 max-w-xl mx-auto">{{$homeInfo->content}}</p>
-                        @else
-                            <h3 class="mb-6 md:text-3xl text-2xl md:leading-normal leading-normal font-semibold">{{__("What is Participatory Budget")}}?</h3>
-                            <p class="text-slate-400 max-w-xl mx-auto">{{__("It's a public policy tool to engage and increase public participation in local government decision-making by funding ideas generated and debated by the population. The Municipality of Lisbon was the first European capital to implement Participatory Budgeting in 2008, through Deliberation No. 506/CM/2008 approved at a City Council meeting, as published on July 9, 2008, in the 3rd supplement to Municipal Bulletin No. 751.")}}</p>
-                        @endif
+            <div class="container relative">
+                <div class="grid grid-cols-1 pb-8 text-center">
+                    @if($homeInfo = \App\Models\HomeInfo::first())
+                        <h3 class="mb-6 md:text-3xl text-2xl md:leading-normal leading-normal font-semibold">{{$homeInfo->title}}?</h3>
+                        <p class="text-slate-400 max-w-xl mx-auto">{{$homeInfo->content}}</p>
+                    @else
+                        <h3 class="mb-6 md:text-3xl text-2xl md:leading-normal leading-normal font-semibold">{{__("What is Participatory Budget")}}?</h3>
+                        <p class="text-slate-400 max-w-xl mx-auto">{{__("It's a public policy tool to engage and increase public participation in local government decision-making by funding ideas generated and debated by the population. The Municipality of Lisbon was the first European capital to implement Participatory Budgeting in 2008, through Deliberation No. 506/CM/2008 approved at a City Council meeting, as published on July 9, 2008, in the 3rd supplement to Municipal Bulletin No. 751.")}}</p>
+                    @endif
 
-                    </div><!--end grid-->
+                </div><!--end grid-->
 
-                    <div class="grid lg:grid-cols-12 md:grid-cols-2 grid-cols-1 items-center mt-10 gap-[30px]">
-                        <div class="lg:col-span-5">
-                            <img src="{{asset('assets-frontend/images/job/job.jpg')}}" alt="">
+                <div class="grid lg:grid-cols-12 md:grid-cols-2 grid-cols-1 items-center mt-10 gap-[30px]">
+                    <div class="lg:col-span-5">
+                        <img src="{{asset('assets-frontend/images/job/job.jpg')}}" alt="">
+                    </div>
+                    <div class="lg:col-span-7">
+                        <div class="lg:ms-10">
+                            <h3 class="mb-6 md:text-3xl text-2xl md:leading-normal leading-normal font-semibold">{{__("Objectives")}}</h3>
+                            <ul class="list-none text-slate-400 mt-4">
+                            @if(\App\Models\HomeBulletPoints::exists())
+                                <?php
+                                    $homeBullets = \App\Models\HomeBulletPoints::get()
+                                ?>
+                                @foreach($homeBullets as $homeBullet)
+                                    <li class="mb-1 flex"><i class="uil uil-check-circle text-indigo-600 text-xl me-2"></i> {{$homeBullet->bullet_point}}</li>
+                                @endforeach
+                            @else
+                                <li class="mb-1 flex"><i class="uil uil-check-circle text-indigo-600 text-xl me-2"></i> {{__("Incentivar o diálogo entre eleitos, técnicos municipais, cidadãos e a sociedade civil organizada, na procura das melhores soluções para os problemas tendo em conta os recursos disponíveis.")}}</li>
+                                <li class="mb-1 flex"><i class="uil uil-check-circle text-indigo-600 text-xl me-2"></i> {{__("Contribuir para a educação cívica, permitindo aos cidadãos integrar as suas preocupações pessoais com o bem comum, compreender a complexidade dos problemas e desenvolver atitudes, competências e práticas de participação.")}}</li>
+                                <li class="mb-1 flex"><i class="uil uil-check-circle text-indigo-600 text-xl me-2"></i> {{__("Adequar as políticas públicas municipais às necessidades e expectativas das pessoas, para melhorar a qualidade de vida na cidade.")}}</li>
+                                <li class="mb-1 flex"><i class="uil uil-check-circle text-indigo-600 text-xl me-2"></i> {{__("Aumentar a transparência da atividade da Autarquia, o nível de responsabilização dos eleitos e da estrutura municipal, contribuindo para reforçar a qualidade da democracia.")}}</li>
+                            @endif
+                            </ul>
                         </div>
-                        <div class="lg:col-span-7">
-                            <div class="lg:ms-10">
-                                <h3 class="mb-6 md:text-3xl text-2xl md:leading-normal leading-normal font-semibold">{{__("Objectives")}}</h3>
-                                <ul class="list-none text-slate-400 mt-4">
-                                @if(\App\Models\HomeBulletPoints::exists())
-                                    <?php
-                                        $homeBullets = \App\Models\HomeBulletPoints::get()
-                                    ?>
-                                    @foreach($homeBullets as $homeBullet)
-                                        <li class="mb-1 flex"><i class="uil uil-check-circle text-indigo-600 text-xl me-2"></i> {{$homeBullet->bullet_point}}</li>
-                                    @endforeach
-                                @else
-                                    <li class="mb-1 flex"><i class="uil uil-check-circle text-indigo-600 text-xl me-2"></i> {{__("Incentivar o diálogo entre eleitos, técnicos municipais, cidadãos e a sociedade civil organizada, na procura das melhores soluções para os problemas tendo em conta os recursos disponíveis.")}}</li>
-                                    <li class="mb-1 flex"><i class="uil uil-check-circle text-indigo-600 text-xl me-2"></i> {{__("Contribuir para a educação cívica, permitindo aos cidadãos integrar as suas preocupações pessoais com o bem comum, compreender a complexidade dos problemas e desenvolver atitudes, competências e práticas de participação.")}}</li>
-                                    <li class="mb-1 flex"><i class="uil uil-check-circle text-indigo-600 text-xl me-2"></i> {{__("Adequar as políticas públicas municipais às necessidades e expectativas das pessoas, para melhorar a qualidade de vida na cidade.")}}</li>
-                                    <li class="mb-1 flex"><i class="uil uil-check-circle text-indigo-600 text-xl me-2"></i> {{__("Aumentar a transparência da atividade da Autarquia, o nível de responsabilização dos eleitos e da estrutura municipal, contribuindo para reforçar a qualidade da democracia.")}}</li>
-                                @endif
-                                </ul>
-                            </div>
-                        </div>
-                    </div><!--end grid-->
-                </div><!--end container-->
-            </section>
-
-            <div class="bg-gray-50 relative flex justify-center text-center">
-                <a href="{{ route('propostas')}}" class="py-2 px-5  w-[450px] inline-block font-semibold tracking-wide border align-middle duration-500 text-center text-xl bg-indigo-600 hover:bg-indigo-800 border-indigo-600 text-white rounded-md">{{__("Participa")}}</a>
-            </div>
+                    </div>
+                </div><!--end grid-->
+            </div><!--end container-->
+        </section>
+        <div class="relative flex justify-center bg-gray-50 text-center pt-20">
+            <a href="{{ route('propostas')}}" class="py-2 px-5  w-[450px] inline-block font-semibold tracking-wide border align-middle duration-50 text-center text-xl bg-indigo-600 hover:bg-indigo-700 text-white rounded-md">{{__("Participa")}}</a>
+        </div>
         <section>
             <div class="relative md:py-24 py-16 bg-gray-50 ">
                 <div class="grid grid-cols-1 text-center">
@@ -88,23 +86,21 @@ $wallpaper = $home->getFirstMediaUrl('wallpaper');
                     <p class="text-slate-400 max-w-xl mx-auto">{{__("Consulte os seguintes Links!")}}</p>
                     <div class="flex flex-row justify-evenly w-1/2 mx-auto">
                         <div class="mt-6">
-                            <a href="{{route('faq-page')}}" class="py-2 px-5 inline-block font-semibold tracking-wide border align-middle duration-500 text-center text-xl bg-indigo-600 hover:bg-indigo-800 border-indigo-600 text-white rounded-md"><i class="uil uil-question-circle text-xl"></i> {{__("FAQ")}}</a>
+                            <a href="{{route('faq-page')}}" class="py-2 px-5 inline-block font-semibold tracking-wide border align-middle duration-50 text-center text-xl bg-indigo-600 hover:bg-indigo-700/80 text-white rounded-md"><i class="uil uil-question-circle text-xl"></i> {{__("FAQ")}}</a>
                         </div>
                         <div class="mt-6">
-                            <a href="{{route('calendar-page')}}" class="py-2 px-5 inline-block font-semibold tracking-wide border align-middle duration-500 text-center text-xl bg-indigo-600 hover:bg-indigo-800 border-indigo-600 text-white rounded-md"><i class="uil uil-question-circle text-xl"></i> {{__("Calendario")}}</a>
+                            <a href="{{route('calendar-page')}}" class="py-2 px-5 inline-block font-semibold tracking-wide border align-middle duration-50 text-center text-xl bg-indigo-600 hover:bg-indigo-700/80 text-white rounded-md"><i class="uil uil-question-circle text-xl"></i> {{__("Calendario")}}</a>
                         </div>
                         <div class="mt-6">
-                            <a href="{{route('rules-page')}}" class="py-2 px-5 inline-block font-semibold tracking-wide border align-middle duration-500 text-center text-xl bg-indigo-600 hover:bg-indigo-800 border-indigo-600 text-white rounded-md"><i class="uil uil-question-circle text-xl"></i> {{__("Regulamento")}}</a>
+                            <a href="{{route('rules-page')}}" class="py-2 px-5 inline-block font-semibold tracking-wide border align-middle duration-50 text-center text-xl bg-indigo-600 hover:bg-indigo-700/80 text-white rounded-md"><i class="uil uil-question-circle text-xl"></i> {{__("Regulamento")}}</a>
                         </div>
                         <div class="mt-6">
-                            <a href="{{route('contact-us')}}" class="py-2 px-5 inline-block font-semibold tracking-wide border align-middle duration-500 text-center text-xl bg-indigo-600 hover:bg-indigo-800 border-indigo-600 text-white rounded-md"><i class="uil uil-question-circle text-xl"></i> {{__("Contact us")}}</a>
+                            <a href="{{route('contact-us')}}" class="py-2 px-5 inline-block font-semibold tracking-wide border align-middle duration-50 text-center text-xl bg-indigo-600 hover:bg-indigo-700/80 text-white rounded-md"><i class="uil uil-question-circle text-xl"></i> {{__("Contact us")}}</a>
                         </div>
 
                     </div>
                 </div><!--end grid-->
             </div><!--end container-->
         </section>
-
-
     </div>
 </x-landing-layout>
